bf9591ac35c578b2f2477a98c3472a04283eb143
Henmyg <github@henmyg.dk>
2025-10-13 21:58:56 +0200

    Add SyncStatus Feature
    Add CommunityToolKit and clean up own Converters
    Make features Views instead of Pages

---
+++ Maui/Maui.Core/Features/SyncStatus/SyncStatusViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Maui.Core.Shared.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.Core.Features.SyncStatus
+{
+    public partial class SyncStatusViewModel: ObservableObject
+    {
+        private readonly PriceSyncService _priceSyncService;
+
+        public SyncStatusViewModel(PriceSyncService priceSyncService)
+        {
+            _priceSyncService = priceSyncService ?? throw new ArgumentNullException(nameof(priceSyncService));
+            _priceSyncService.PropertyChanged += OnPriceSyncServicePropertyChanged;
+        }
+
+        public bool IsSynching => _priceSyncService.IsSynching;
+        public string SyncText => IsSynching ? "Syncing" : "Up-to-date";
+
+        private void OnPriceSyncServicePropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(PriceSyncService.IsSynching))
+            {
+                OnPropertyChanged(nameof(IsSynching));
+                OnPropertyChanged(nameof(SyncText));
+            }
+        }
+    }
+}
---
+++ Maui/Maui/Features/SyncStatus/SyncStatusView.xaml.cs
@@ -0,0 +1,11 @@
+using Maui.Core.Features.SyncStatus;
+
+namespace Maui.Features.SyncStatus;
+
+public partial class SyncStatusView : ContentView
+{
+	public SyncStatusView()
+	{
+		InitializeComponent();
+	}
+}
