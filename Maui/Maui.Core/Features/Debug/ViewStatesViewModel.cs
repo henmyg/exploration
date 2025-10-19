32953b26bdc5afff5fbb5ce5407d1ae35235d617
Henmyg <github@henmyg.dk>
2025-10-19 20:03:06 +0200

    Use PageModel for ViewStatesPage

---
+++ Maui/Maui.Core/Features/Debug/ViewStatesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Maui.Core.Shared.Services;
+using System.Windows.Input;
+
+namespace Maui.Core.Features.Debug
+{
+    public partial class ViewStatesPageModel: ObservableObject
+    {
+        [ObservableProperty]
+        private ICommand _goToSyncStatusCommand;
+
+        public ViewStatesPageModel(INavigationService navigationService)
+        {
+            _goToSyncStatusCommand = new RelayCommand(async () => await navigationService.GoToAsync("debug/viewstates/syncstatus"));
+        }
+    }
+}
