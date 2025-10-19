48d45dbae044350a20df17bd1086d554fcc222c2
Henmyg <github@henmyg.dk>
2025-10-19 14:07:20 +0200

    Use Template for Preview views

---
+++ Maui/Maui.Core/Features/Debug/IPreviewView.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.Core.Features.Debug;
+
+public interface IPreviewView<TState> where TState : Enum
+{
+    public TState ViewState { get; set; }
+    public IReadOnlyList<ViewStateOption<TState>> States {  get; }
+}
+
+public record ViewStateOption<TEnum>(string Title, TEnum Value) where TEnum : Enum;
---
+++ Maui/Maui/Features/Debug/ViewStates/PreviewTemplate.xaml.cs
@@ -0,0 +1,9 @@
+namespace Maui.Features.Debug.ViewStates;
+
+public partial class PreviewTemplate : ResourceDictionary
+{
+	public PreviewTemplate()
+	{
+		InitializeComponent();
+	}
+}
